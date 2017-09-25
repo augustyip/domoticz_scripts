9ee9efa680b045985ca8cd7512f818b8b6868dc5
August Yip <augustyip@gmail.com>
2017-09-25 14:30:26 +0800

    update vue app

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,13 +1,28 @@
 import Vue from 'vue'
-import App from './App.vue'
 
-new Vue({
+
+var domoticzEndpoint = 'http://augustyip.mynetgear.com:58080/json.htm?type=devices&rid=13'
+var app = new Vue({
   el: '#app',
-  // render: h => h(App)，
   data: {
-    temperature: {
-      current: 36
-    }
+    items : []
+  },
+  methods: {
+    loadData: function () {
+      this.$http.get(domoticzEndpoint).then(response => {
+        // get body data
+        this.items = response.json;
+    
+      }, response => {
+        // error callback
+      });
   }
+  },
+  ready: function () {
+    this.loadData();
 
+    setInterval(function () {
+      this.loadData();
+    }.bind(this), 30000); 
+  }
 })
